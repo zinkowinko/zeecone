f19e3f9c4c92ddb1a38d32be4bac9cdc7a7a136a
zinko <projectrez69420@gmail.com>
2025-05-10 12:26:07 -0700

    Update and rename _apps.js to _app.js

--- styles/_app.js
+++ styles/_app.js
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "@/styles/globals.css"; // Adjust if your CSS is somewhere else
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
